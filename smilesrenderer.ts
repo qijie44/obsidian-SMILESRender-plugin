31bb99796ba3d9b1a8f60f1c3f503a6ed0ad1e87
Yeow Qi Jie <qijie44@gmail.com>
2022-10-09 19:29:09 +0800

    playing with the editor, incomplete build

--- smilesrenderer.ts
+++ smilesrenderer.ts
@@ -1,5 +1,5 @@
 import { MarkdownRenderChild } from "obsidian";
-import { Molecule, StructureView } from "openchemlib/full"
+import { Molecule, StructureView, StructureEditor } from "openchemlib/full"
 
 export class Renderer extends MarkdownRenderChild {
   text: string;
@@ -12,10 +12,18 @@ export class Renderer extends MarkdownRenderChild {
 
   onload() {
     if (this.text){
-      const Mol = Molecule.fromSmiles(this.text);
-      const molEl = this.containerEl.createEl("canvas", { cls: "molecule_canvas" });
-      StructureView.drawMolecule(molEl, Mol);
-      this.containerEl.replaceWith(molEl);
+      // const Mol = Molecule.fromSmiles(this.text);
+      // const molEl = this.containerEl.createEl("canvas", { cls: "molecule_canvas" });
+      // StructureView.drawMolecule(molEl, Mol);
+      // this.containerEl.replaceWith(molEl);
+    
+      const ed = StructureEditor.createEditor("editor");
+      // ed.setChangeListenerCallback((a, molecule) => {
+      //   console.log(molecule.toSmiles());
+      // });
+      console.log(this.text);
+      ed.setSmiles(this.text);
+      this.containerEl.replaceWith(ed);
     }
   }
 
